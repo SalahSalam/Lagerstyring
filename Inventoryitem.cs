990e0dfc268373299965d7712fca025c450307da
Salah Eddin Salam <144003465+SalahSalam@users.noreply.github.com>
2025-12-03 17:37:32 +0100

    Classes

---
+++ Inventoryitem.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagerstyring
+{
+    public class Inventoryitem
+    {
+        private int InventoryItemID { get; }
+        private int ProductID { get; }
+        private int Locationid { get; }
+        private int Quantity { get; set; }
+        private DateTime LastUpdated { get; set; }
+    }
+}
---
+++ Location.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lagerstyring
+{
+    public class Location
+    {
+        private int LocationID { get; }
+        private string Name { get; set; }
+        private string Description { get; set; }
+    }
+}
